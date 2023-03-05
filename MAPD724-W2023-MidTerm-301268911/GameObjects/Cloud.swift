621dedd0d2f3e7f3901fbc660997f1b2016d026d
Kisu <krishna@Krishnas-MacBook-Air.local>
2023-03-05 16:04:00 -0500

    2 clouds addedd

--- MAPD724-W2023-MidTerm-301268911/GameObjects/Cloud.swift
+++ MAPD724-W2023-MidTerm-301268911/GameObjects/Cloud.swift
@@ -31,7 +31,7 @@ class Cloud : GameObject
     
     override func CheckBounds()
     {
-        if(position.y <= -902)
+        if(position.x <= -902)
         {
             Reset()
         }
@@ -40,18 +40,18 @@ class Cloud : GameObject
     override func Reset()
     {
         // randomize the vertical speed: 5 to 10
-        verticalSpeed = CGFloat((randomSource?.nextUniform())! * 5.0) + 5.0
+        verticalSpeed = CGFloat((randomSource?.nextUniform())! * -4.0) + 2.0
         
         // randomize the horizontal speed: -2 to 2
-        horizontalSpeed = CGFloat((randomSource?.nextUniform())! * 4.0) - 2.0 
+        horizontalSpeed = CGFloat((randomSource?.nextUniform())! * 5.0) + 5.0
         
-        // get a pseudo random number for the X position: -262 to 262
-        let randomX:Int = (randomSource?.nextInt(upperBound: 524))! - 262
-        position.x = CGFloat(randomX)
-        
-        // get a pseudo random number for the Y position: 902 to 932
-        let randomY:Int = (randomSource?.nextInt(upperBound: 30))! + 902
+        // get a pseudo random number for the Y position: -262 to 262
+        let randomY:Int = (randomSource?.nextInt(upperBound: 524))! - 262
         position.y = CGFloat(randomY)
+        
+        // get a pseudo random number for the X position: 902 to 932
+        let randomX:Int = (randomSource?.nextInt(upperBound: 30))! + 902
+        position.x = CGFloat(randomX)
         
         isColliding = false
     }
